d09439803d9c5b79d241674c3506113c4eab5214
tha7 <tha7@uwo.ca>
2023-08-10 19:10:24 -0400

    minor changes

--- src/NewTodoForm.js
+++ src/NewTodoForm.js
@@ -6,22 +6,21 @@ export function NewTodoForm({ onSubmit }) {
   function handleSubmit(e) {
     e.preventDefault()
     if (newItem === "") return
-
     onSubmit(newItem)
-
     setNewItem("")
   }
 
   return (
     <form onSubmit={handleSubmit} className="new-item-form">
       <div className="form-row">
-        <h1 htmlFor="item">What do you want to accomplish?</h1>
+        <h1 htmlFor="item" style={{fontSize: "2em", textAlign:"center"}}>What do you want to accomplish?</h1>
         <hr class="divider"/>
         <input
           value={newItem}
           onChange={e => setNewItem(e.target.value)}
           type="text"
           id="item"
+          placeholder="What's your task?"
         />
       </div>
       <button className="btn">Add Task</button>
